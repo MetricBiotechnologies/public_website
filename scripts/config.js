8704235b25eef84668bcd53bf1c30c987e82bc85
Chaitanya Rastogi <crastogi@gmail.com>
2026-05-21 18:02:43 -0400

    Wire up Formspree contact form endpoint

--- scripts/config.js
+++ scripts/config.js
@@ -2,5 +2,5 @@
 // Default: Formspree (free; sign up at https://formspree.io and replace FORM_ID below).
 // Alternative: a Hostinger-hosted PHP endpoint like "/contact.php".
 window.METRIC_CONFIG = {
-  formEndpoint: "https://formspree.io/f/REPLACE_WITH_FORM_ID"
+  formEndpoint: "https://formspree.io/f/mbdboqbq"
 };
